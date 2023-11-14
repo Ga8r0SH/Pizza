3d57fd6e34763771c5d0cfbe6f1ecbf927b25f62
Stepan <stepan.gavriusenco@gmail.com>
2023-11-15 00:31:58 +0200

    v.1.6

--- src/components/Preloader/index.jsx
+++ src/components/Preloader/index.jsx
@@ -3,7 +3,9 @@ import './Preloader.css';
 const Preloader = () => {
     return (
         <>
-            <div className="loader">
+            <div className="loader-wrapper">
+                <div className="loader">
+                </div>
             </div>
         </>
     )
