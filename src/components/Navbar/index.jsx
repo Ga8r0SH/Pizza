7f709d5bd837e23abea6853d52f0b81bfcd10305
Stepan <stepan.gavriusenco@gmail.com>
2023-11-14 23:57:43 +0200

    v.1.5

--- src/components/Navbar/index.jsx
+++ src/components/Navbar/index.jsx
@@ -49,9 +49,9 @@ const Navbar = ({ navBarSort, navBarSortByRating }) => {
     return (
 
         <div className="content__top">
-            <div class="burger-menu" onClick={() => handleClickFilterBurger()}>
-                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width='24px'>
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
+            <div className="burger-menu" onClick={() => handleClickFilterBurger()}>
+                <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width='24px'>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M3.75 6.75h16.5M3.75 12h16.5m-16.5 5.25h16.5" />
                 </svg>
             </div>
             <div className={`categories ${filterBurger ? 'active' : ''}`}>
@@ -95,8 +95,8 @@ const Navbar = ({ navBarSort, navBarSortByRating }) => {
                     ) : null}
 
                 </div>
-                <svg className='burgerIcon' onClick={handleClickRatingBurger} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" width='24px' height='24px'>
-                    <path stroke-linecap="round" stroke-linejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
+                <svg className='burgerIcon' onClick={handleClickRatingBurger} xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth="1.5" stroke="currentColor" width='24px' height='24px'>
+                    <path strokeLinecap="round" strokeLinejoin="round" d="M12 3c2.755 0 5.455.232 8.083.678.533.09.917.556.917 1.096v1.044a2.25 2.25 0 01-.659 1.591l-5.432 5.432a2.25 2.25 0 00-.659 1.591v2.927a2.25 2.25 0 01-1.244 2.013L9.75 21v-6.568a2.25 2.25 0 00-.659-1.591L3.659 7.409A2.25 2.25 0 013 5.818V4.774c0-.54.384-1.006.917-1.096A48.32 48.32 0 0112 3z" />
                 </svg>
             </div>
         </div>
